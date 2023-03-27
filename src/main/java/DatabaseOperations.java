5666c1c5dc74c9aab1fff639ea779b1e75dc4b59
dbasumatary <basudhondip@gmail.com>
2023-03-28 01:45:32 +0530

    Added: Method to update basic pay and sync it with database

--- src/main/java/DatabaseOperations.java
+++ src/main/java/DatabaseOperations.java
@@ -4,13 +4,13 @@ import java.sql.*;
 
 public class DatabaseOperations {
     public Connection getConnection() throws SQLException{
-        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?allowPublicKeyRetrieval=true&useSSL=false";
         String userName = "root";
         String password = "DBUnited23@sql";
         Connection connection;
-        System.out.println("Connecting to database ");
+        System.out.println("Connecting to database...");
         connection = DriverManager.getConnection(jdbcURL,userName,password);
-        System.out.println("Connection is Successful "+ connection);
+        System.out.println("Connection is Successful to connection "+ connection);
         System.out.println();
         return connection;
     }
@@ -39,25 +39,59 @@ public class DatabaseOperations {
                 System.out.println("Income Tax : "+incomeTax);
                 System.out.println("Net Pay : "+netPay);
                 System.out.println("Start Date : "+startDate);
-                System.out.println("*************************************");
+                System.out.println("*****************************************");
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
+    //UC3 - Updating the salary
+    public void updateBasicPay(String name, double newBasicPay){
+        try {
+            Connection connection = this.getConnection();
+
+            //The query updates the Basic_Pay column of the employee_payroll table for the employee with specified Name.
+            String query = "UPDATE employee_payroll SET Basic_Pay = ? WHERE Name = ?";
+
+            //The PreparedStatement object is used to set the new value of basicPay and name
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setDouble(1, newBasicPay);
+            statement.setString(2, name);
+
+            //the executeUpdate() method returns the number of rows affected by the update.
+            int rowsAffected = statement.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Basic Pay updated successfully for employee with name " + name);
+            } else {
+                System.out.println("No employee found with name " + name);
+            }
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) throws Exception{
+        DatabaseOperations databaseOperations = new DatabaseOperations();
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int flag = -1;
         while (flag != 0){
+            System.out.println("*****************************************");
             System.out.println("Enter your choice:");
-            System.out.println("\n1. Display employee details\n2. Exit");
+            System.out.println("\n1. Display employee details\n2. Update basic pay\n3. Exit");
             flag = Integer.parseInt(bufferedReader.readLine());
             switch (flag){
-                case 1: DatabaseOperations databaseOperations = new DatabaseOperations();
-                        databaseOperations.displayDetails();
+                case 1: databaseOperations.displayDetails();
                         break;
-                case 2: flag = 0;
+                case 2: System.out.print("Enter the name: ");
+                        String name = bufferedReader.readLine();
+                        System.out.print("Enter the updated basic pay for " + name + ": ");
+                        Double newPay = Double.parseDouble(bufferedReader.readLine());
+                        databaseOperations.updateBasicPay(name,newPay);
+                        break;
+                case 3: flag = 0;
                         break;
             }
         }
